23ddb3c60bcc08f6e8a0dcc7fc3b954326022d1c
dhubleur <dhubleur@student.42.fr>
2022-02-21 14:37:45 +0100

    absoltue and relative path

--- execution/main.c
+++ execution/main.c
@@ -6,29 +6,28 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 17:52:56 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/02/21 13:45:50 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/02/21 14:28:33 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands.h"
-#include "get_next_line.h"
 
 static t_command_list *get_fake_command_list()
 {
 	t_command_list *list1 = malloc(sizeof(t_command_list));
 	t_command *command1 = malloc(sizeof(t_command));
-	command1->name = strdup("cat");
+	command1->name = strdup("../a.out");
 	command1->args = malloc(sizeof(char*) * 1);
 	command1->args[0] = NULL;
 	command1->redirection_count = 0;
 	command1->redirections = NULL;
 	list1->command = command1;
 	list1->next = NULL;
-	list1->separator = PIPE;
+	list1->separator = END;
 
 	t_command_list *list2 = malloc(sizeof(t_command_list));
 	t_command *command2 = malloc(sizeof(t_command));
-	command2->name = strdup("cat");
+	command2->name = strdup("wc");
 	command2->args = malloc(sizeof(char*) * 1);
 	command2->args[0] = NULL;
 	command2->redirection_count = 0;
@@ -36,7 +35,7 @@ static t_command_list *get_fake_command_list()
 	list2->command = command2;
 	list2->next = NULL;
 	list2->separator = PIPE;
-	list1->next = list2;
+	//list1->next = list2;
 
 	t_command_list *list3 = malloc(sizeof(t_command_list));
 	t_command *command3 = malloc(sizeof(t_command));
@@ -48,31 +47,32 @@ static t_command_list *get_fake_command_list()
 	list3->command = command3;
 	list3->next = NULL;
 	list3->separator = END;
-	list2->next = list3;
+	//list2->next = list3;
 
 	return list1;
 }
 
 char	*get_path_from_env(char *command_name)
 {
-	char *path = strdup("/usr/sbin:/usr/bin");
-	int	 found;
-	char *exec_path;
+	char	*path = strdup("/usr/sbin:/usr/bin");
+	int		found;
+	char	*exec_path;
 
 	found = 0;
 	exec_path = NULL;
-	if(!path)
-		return NULL;
+	if (!path)
+		return (NULL);
 	while (ft_strtrunc(&path, ':'))
 	{
-		if(!found)
+		if (!found)
 		{
-			exec_path = malloc(sizeof(char) * (strlen(path) + strlen(command_name) + 2));
+			exec_path = malloc(
+					sizeof(char) * (strlen(path) + strlen(command_name) + 2));
 			exec_path[0] = '\0';
 			strcat(exec_path, path);
 			strcat(exec_path, "/");
 			strcat(exec_path, command_name);
-			if(access(exec_path, F_OK) == 0)
+			if (access(exec_path, F_OK) == 0)
 				found = 1;
 			else
 			{
@@ -81,23 +81,7 @@ char	*get_path_from_env(char *command_name)
 			}
 		}
 	}
-	return exec_path;
-}
-
-char	*get_path_from_pwd(char *command_name)
-{
-	char *pwd = strdup("/mnt/d/root/Files/Dev/C/42-minishell/execution");
-	char *exec_path;
-
-	exec_path = malloc(sizeof(char) * (strlen(pwd) + strlen(command_name) + 2));
-	exec_path[0] = '\0';
-	strcat(exec_path, pwd);
-	strcat(exec_path, "/");
-	strcat(exec_path, command_name);
-	if(access(exec_path, F_OK) == 0)
-		return exec_path;
-	free(exec_path);
-	return NULL;
+	return (exec_path);
 }
 
 void	add_path_to_args(t_command *command, char *path)
@@ -108,12 +92,12 @@ void	add_path_to_args(t_command *command, char *path)
 
 	args = command->args;
 	length = 0;
-	while(args[length])
+	while (args[length])
 		length++;
 	command->args = malloc(sizeof(char *) * (length + 2));
 	command->args[0] = path;
 	i = -1;
-	while(++i < length)
+	while (++i < length)
 		command->args[i + 1] = args[i];
 	free(args);
 }
@@ -123,99 +107,89 @@ int	execute_file(t_command *command, char *path, char **envp, int is_piped)
 	pid_t	pid;
 	int		pipefd[2];
 	int		wait_status;
-	
-	printf("Execute: %s\n", path);
+
 	add_path_to_args(command, path);
-	if(is_piped && pipe(pipefd) < 0)
-		return -1;
+	if (is_piped && pipe(pipefd) < 0)
+		return (-1);
 	pid = fork();
-	if(pid == -1)
-		return -1;
-	if(is_piped)
+	if (pid == -1)
+		return (-1);
+	if (is_piped)
 	{
-		if(pid == 0)
+		if (pid == 0)
 		{
-			//printf("Redirect %s stdout to pipe\n", command->name);
 			dup2(pipefd[1], 1);
 			close(pipefd[0]);
 			close(pipefd[1]);
 		}
 		else
 		{
-			//printf("Redirect stdin of %s + 1 command from pipe\n", command->name);
 			dup2(pipefd[0], 0);
 			close(pipefd[1]);
 			close(pipefd[0]);
 		}
 	}
-	if(pid == 0)
+	if (pid == 0)
 	{
 		execve(path, command->args, envp);
 		perror("Execution error");
 		exit(errno);
 	}
 	else
-	{
-		return 1;
-	}
+		return (1);
 }
 
 int	execute(t_command *command, t_command_separator separator_to_next, char **envp)
 {
 	char	*path;
-	
-	printf("----------\nExecuting \'%s\' with args:\n", command->name);
-	for(int i = 0; command->args[i]; i++)
-		printf("- \'%s\'\n", command->args[i]);
-	if(strchr(command->name, '/') == NULL)
+
+	if (strchr(command->name, '/') == NULL)
 	{
-		if(is_builtin(command->name))
+		if (is_builtin(command->name))
 		{
 			printf("Execute builtin\n");
 		}
 		else
 		{
-			printf("Execute from $PATH\n");
 			path = get_path_from_env(command->name);
-			if(path == NULL)
+			if (path == NULL)
 			{
 				printf("%s: command not found\n", command->name);
-				return -1;
+				return (-1);
 			}
 			else
-				return(execute_file(command, path, envp, separator_to_next == PIPE));
+				return (execute_file(
+						command, path, envp, separator_to_next == PIPE));
 		}
 	}
 	else
 	{
-		path = get_path_from_pwd(command->name);
-		if(path == NULL)
-		{
-			printf("Minishell: %s: No such file or directory\n", command->name);
-			return -1;
-		}
-		else
-			return(execute_file(command, path, envp, separator_to_next == PIPE));
+		if (access(command->name, F_OK) == 0)
+			return (execute_file(
+					command, command->name, envp, separator_to_next == PIPE));
+		printf("Minishell: %s: No such file or directory\n", command->name);
+		return (-1);
 	}
 }
 
 int execute_list(t_command_list **list, char **envp)
 {
 	t_command_list	*command_elem;
-	int count;
-	int i;
-	int	status;
-	char	*str;
+	int 			count;
+	int 			i;
+	int				status;
+	char			*str;
 
 	count = 0;
 	command_elem = *list;
-	while(command_elem != NULL)
+	while (command_elem != NULL)
 	{
-		count += (execute(command_elem->command, command_elem->separator, envp) == 1);
+		count += (execute(
+					command_elem->command, command_elem->separator, envp) == 1);
 		command_elem = command_elem->next;
 	}
 	i = -1;
-	while(++i < count)
+	while (++i < count)
 	{
 		waitpid(-1, &status, 0);
 		close(0);
@@ -224,6 +198,7 @@ int execute_list(t_command_list **list, char **envp)
 
 int main(int argc, char **argv, char **envp)
 {
-	t_command_list *list = get_fake_command_list();
+	t_command_list	*list = get_fake_command_list();
+
 	execute_list(&list, envp);
 }
