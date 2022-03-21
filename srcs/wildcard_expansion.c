6a7b224885c3fab725e58fd11c869a8aa23955d0
noctuelles <plouvel@student.42.fr>
2022-03-21 18:25:28 +0100

    finished stage 1 of expansion

---
+++ srcs/wildcard_expansion.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard_expansion.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 14:50:38 by plouvel           #+#    #+#             */
+/*   Updated: 2022/03/21 18:19:06 by plouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <dirent.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "minishell.h"
+#include "ft_dprintf.h"
+
+/* merge_list_content() merge all the value in the double linked list in the
+ * string str. */
+
+static void	merge_list_content(char *str, t_dlist *files)
+{
+	char	*filename;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (files)
+	{
+		j = 0;
+		filename = (char *) files->content;
+		while (filename[j] != '\0')
+			str[i++] = filename[j++];
+		if (files->next == NULL)
+			str[i] = '\0';
+		else
+			str[i++] = ' ';
+		files = files->next;
+	}
+}
+
+static char	*list_to_string(t_dlist *files)
+{
+	size_t	str_size;
+	char	*str;
+
+	if (files == NULL)
+		return (NULL);
+	ascii_sort_list(files);
+	str_size = compute_str_size(files);
+	str = (char *) malloc(str_size * sizeof(char));
+	if (!str)
+		return (NULL);
+	merge_list_content(str, files);
+	return (str);
+}
+
+static int	scan_current_directory(char **str, char *pattern)
+{
+	DIR				*dir_stream;
+	struct dirent	*dir_ent;
+	t_dlist			*files;
+
+	errno = 0;
+	files = NULL;
+	dir_stream = opendir(CURRENT_DIR);
+	if (!dir_stream)
+		ft_dprintf(STDERR_FILENO, STR_ERROR_M, STR_OPENDIR, strerror(errno));
+	while (dir_stream)
+	{
+		dir_ent = readdir(dir_stream);
+		if (!dir_ent)
+			break ;
+		if (add_file_to_list(&files,
+					dir_ent->d_name, pattern, dir_ent->d_type) == -1)
+				break ;
+	}
+	if (dir_stream && errno != ENO)
+		ft_dprintf(STDERR_FILENO, STR_ERROR_M, STR_READDIR, strerror(errno));
+	else
+		*str = list_to_string(files);
+	ft_dlstclear(&files, free);
+	closedir(dir_stream);
+	return (errno);
+}
+
+/* wildcard_expansion() returns a string if one or more files are matching the
+ * pattern. If no files are found or an error occured, the function returns
+ * NULL.
+ * This string contains each filename separated by space by ascii order. */
+
+char	*wildcard_expansion(char *pattern)
+{
+	char *str;
+
+	str = NULL;
+	scan_current_directory(&str, pattern);
+	return (str);
+}
---
+++ srcs/wildcard_expansion_utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard_expansion_utils.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 15:56:09 by plouvel           #+#    #+#             */
+/*   Updated: 2022/03/21 18:22:16 by plouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include "libft.h"
+
+/* match_pattern() returns true if filename is respecting the pattern, else
+ * , it returns false.
+ * The algorithm is both iterative and recursive.
+ * */
+
+static bool	match_pattern(char *filename, char *pattern)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (pattern[j] && pattern[j] != '*')
+	{
+		if (filename[i++] != pattern[j++])
+			return (false);
+	}
+	if (filename[i] == '\0' && pattern[j] == '\0')
+		return (true);
+	while (pattern[j + 1] == '*')
+		j++;
+	if (pattern[j + 1] == '\0')
+		return (true);
+	while (filename[i] && filename[i] != pattern[j + 1])
+		i++;
+	if (filename[i] == '\0')
+		return (false);
+	return (match_pattern(&filename[i], &pattern[j + 1]));
+}
+
+int	add_file_to_list(t_dlist **files, char *filename, char *pattern,
+		unsigned char d_type)
+{
+	t_dlist	*elem;
+	char	*filename_dup;
+
+	if (d_type != DT_UNKNOWN && filename[0] != '.')
+	{
+		if (match_pattern(filename, pattern) == true)
+		{
+			filename_dup = ft_strdup(filename);
+			if (!filename_dup)
+				return (-1);
+			elem = ft_dlstnew((void *) filename_dup);
+			if (!elem)
+			{
+				free(filename_dup);
+				return (-1);
+			}
+			ft_dlstadd_back(files, elem);
+		}
+	}
+	return (0);
+}
+
+/* ascii_sort_list() sorts the list in ascii orderfiles using insert sort
+ * algorithm. */
+
+void	ascii_sort_list(t_dlist *files)
+{
+	char	*curr_filename;
+
+	files = files->next;
+	while (files)
+	{
+		curr_filename = files->content;
+		while (files->prev != NULL
+				&& ft_strcmp(files->prev->content, curr_filename) > 0)
+		{
+			files->content = files->prev->content;
+			files = files->prev;
+		}
+		files->content = curr_filename;
+		files = files->next;
+	}
+}
+
+size_t	compute_str_size(t_dlist *files)
+{
+	size_t	str_size;
+
+	str_size = 0;
+	while (files)
+	{
+		str_size += ft_strlen(files->content) + 1;
+		files = files->next;
+	}
+	return (str_size);
+}
