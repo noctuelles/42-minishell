fad762e2216e137cf3c68c0a8d7f6accb434eac1
noctuelles <plouvel@student.42.fr>
2022-04-02 18:43:07 +0200

    beginning of the work

--- srcs/var_expansion.c
+++ srcs/var_expansion.c
@@ -6,7 +6,7 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 17:34:49 by plouvel           #+#    #+#             */
-/*   Updated: 2022/04/02 18:31:57 by plouvel          ###   ########.fr       */
+/*   Updated: 2022/04/02 18:38:55 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ static void update_quote(char *str, char *quote)
 		*quote = *str;
 	else if (*str == *quote)
 		*quote = '\0';
+}
+
+static t_elem	*tokenise_expansion(t_lexer *lexer, t_dlist *elem, t_token *tkn)
+{
+	t_dlist	*tkn_list;
+
 }
 
 t_token	*var_expansion(t_token *tkn, t_dlist *env_var)
