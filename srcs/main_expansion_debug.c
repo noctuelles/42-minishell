d115886929e0b33f720f5be85a25b5f447c56fcb
noctuelles <plouvel@student.42.fr>
2022-04-15 16:33:34 +0200

    added node_expansion.c function

--- srcs/main_expansion_debug.c
+++ srcs/main_expansion_debug.c
@@ -114,16 +114,9 @@ int main(int argc, char **argv, char **envp)
 				{
 					ft_printf("\n{1;4;33}Initial tree :{0}\n\n");
 					ast_print_tree("", root, false);
-					t_dlist	*elem = root->args;
-					while (elem)
-					{
-						t_arg *arg = elem->content;
-						elem = var_expansion(&root->args, elem, arg, env_var);
-						if (elem)
-							elem = elem->next;
-						else
-							break ;
-					}
+					apply_expansion_on_node(root, env_var);
+					ft_printf("\n{1;4;33}Expanded tree :{0}\n\n");
+					ast_print_tree("", root, false);
 					ast_tree_delete_node(root);
 					/*if (root)
 					{
