795f4ac487cab6942a7b490f012aeb12ce7b4b2b
damien <dhubleur@student.42.fr>
2022-04-19 12:22:43 +0200

    repair cd with env -i

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,30 +6,32 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 18:04:21 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/04/19 11:08:53 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/04/19 12:22:01 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "ft_printf.h"
 
-void	update_env(char *pwd, char *old_pwd, t_dlist *env)
+void	update_env(char *pwd, char *old_pwd, t_minishell *minishell)
 {
-	if (get_var(env, "OLDPWD") != NULL)
+	if (get_var(minishell->vars, "OLDPWD") != NULL)
 	{
-		free(get_var(env, "OLDPWD")->value);
-		get_var(env, "OLDPWD")->value = old_pwd;
+		free(get_var(minishell->vars, "OLDPWD")->value);
+		get_var(minishell->vars, "OLDPWD")->value = old_pwd;
 	}
 	else
-		import_one_var(&env, ft_strcat(ft_strdup("OLDPWD="), old_pwd));
-	get_var(env, "OLDPWD")->value_len = ft_strlen(old_pwd);
-	if (get_var(env, "PWD") != NULL)
 	{
-		get_var(env, "PWD")->value = pwd;
+		add_var(&(minishell->vars), "OLDPWD", old_pwd);
+	}
+	get_var(minishell->vars, "OLDPWD")->value_len = ft_strlen(old_pwd);
+	if (get_var(minishell->vars, "PWD") != NULL)
+	{
+		get_var(minishell->vars, "PWD")->value = pwd;
 	}
 	else
-		import_one_var(&env, ft_strcat(ft_strdup("PWD="), pwd));
-	get_var(env, "PWD")->value_len = ft_strlen(pwd);
+		add_var(&(minishell->vars), "PWD", pwd);
+	get_var(minishell->vars, "PWD")->value_len = ft_strlen(pwd);
 }
 
 int	ft_cd(int argc, char **argv, t_minishell *minishell)
@@ -52,7 +54,7 @@ int	ft_cd(int argc, char **argv, t_minishell *minishell)
 			ft_printf(ERROR_ERRNO, argv[0], strerror(errno));
 			return (1);
 		}
-		update_env(pwd, old_pwd, env);
+		update_env(pwd, old_pwd, minishell);
 		return (0);
 	}
 	ft_printf(ERROR_ERRNO, argv[0], strerror(errno));
--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 14:19:13 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/04/19 10:59:13 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/04/19 12:09:56 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ int	ft_pwd(int argc, char **argv, t_minishell *minishell)
 {
 	(void)argc;
 	(void)argv;
-	ft_printf("%s\n", get_var(minishell->vars, "PWD")->value);
+	if (get_var(minishell->vars, "PWD") != NULL)
+		ft_printf("%s\n", get_var(minishell->vars, "PWD")->value);
+	else
+		ft_printf("minishell: unable to locate current directory\n");
 	return (0);
 }
