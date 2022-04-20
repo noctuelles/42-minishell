6392607ae7ca4cb50ae8e72c3722af56a83d7224
damien <dhubleur@student.42.fr>
2022-04-20 15:31:16 +0200

    normalize messages

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 13:14:31 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/04/18 13:45:02 by dhubleur         ###   ########.fr       */
+/*   Updated: 2022/04/20 15:30:42 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@
 
 # include "env.h"
 
-# define CD_FORMAT_ERROR "Minishell: %s: use with format: %s <relative or \
+# define CD_FORMAT_ERROR "minishell: %s: use with format: %s <relative or \
 absolute path>\n"
-# define EXIT_NUMERIC_REQUIRED "Minishell: %s: %s: numeric argument required\n"
-# define EXIT_TO_MANY "Minishell: %s: too many arguments\n"
+# define EXIT_NUMERIC_REQUIRED "minishell: %s: %s: numeric argument required\n"
+# define EXIT_TO_MANY "minishell: %s: too many arguments\n"
 # define EXPORT_LIST "declare -x %s=\""
-# define UNSET_NOT_ENOUGH "Minishell: %s: not enough arguments\n"
-# define ERROR_ERRNO "Minishell: %s: %s\n"
+# define UNSET_NOT_ENOUGH "minishell: %s: not enough arguments\n"
+# define ERROR_ERRNO "minishell: %s: %s\n"
 
 int		ft_cd(int argc, char **argv, t_minishell *minishell);
 int		ft_echo(int argc, char **argv, t_minishell *minishell);
